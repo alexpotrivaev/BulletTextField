e82e931e079551ed307950a9955714484933145e
Alex Potrivaev <alexpotrivaev@gmail.com>
2023-05-14 11:20:50 +0100

    Initial Commit

---
+++ BulletTextField/BulletTextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  BulletTextFieldApp.swift
+//  BulletTextField
+//
+//  Created by Alex Potrivaev on 14/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct BulletTextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
